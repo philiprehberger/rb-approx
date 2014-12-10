ecdab36c1c38378ec14ef22807971fbce0332e22
Reverend Philip <me@philiprehberger.com>
2014-12-10 00:11:07 -0600

    Release v0.1.2

--- spec/philiprehberger/approx_spec.rb
+++ spec/philiprehberger/approx_spec.rb
@@ -51,11 +51,107 @@ RSpec.describe Philiprehberger::Approx do
     it 'falls back to == for non-numeric types' do
       expect(described_class.equal?('hello', 'hello')).to be true
     end
+
+    it 'returns true for identical integers' do
+      expect(described_class.equal?(5, 5)).to be true
+    end
+
+    it 'returns false for different integers' do
+      expect(described_class.equal?(5, 6)).to be false
+    end
+
+    it 'compares integer and float within epsilon' do
+      expect(described_class.equal?(1, 1.0)).to be true
+    end
+
+    it 'handles zero values' do
+      expect(described_class.equal?(0.0, 0.0)).to be true
+    end
+
+    it 'handles zero tolerance' do
+      expect(described_class.equal?(1.0, 1.0, epsilon: 0.0)).to be true
+    end
+
+    it 'returns false for very small difference with zero tolerance' do
+      expect(described_class.equal?(1.0, 1.0 + 1e-15, epsilon: 0.0)).to be false
+    end
+
+    it 'handles negative numbers' do
+      expect(described_class.equal?(-1.0, -1.0 + 1e-10)).to be true
+    end
+
+    it 'handles negative and positive near zero' do
+      expect(described_class.equal?(-1e-10, 1e-10)).to be true
+    end
+
+    it 'returns false for negative numbers far apart' do
+      expect(described_class.equal?(-1.0, -2.0)).to be false
+    end
+
+    it 'handles very large numbers' do
+      expect(described_class.equal?(1e15, 1e15 + 1e-10)).to be true
+    end
+
+    it 'handles very small numbers' do
+      expect(described_class.equal?(1e-15, 2e-15, epsilon: 1e-14)).to be true
+    end
+
+    it 'handles Float::NAN (NaN is not near NaN)' do
+      # NaN - NaN is NaN, .abs is NaN, NaN <= epsilon is false
+      expect(described_class.equal?(Float::NAN, Float::NAN)).to be false
+    end
+
+    it 'handles Float::INFINITY' do
+      # Infinity - Infinity is NaN
+      expect(described_class.equal?(Float::INFINITY, Float::INFINITY)).to be false
+    end
+
+    it 'returns false for infinity vs finite' do
+      expect(described_class.equal?(Float::INFINITY, 1.0)).to be false
+    end
+
+    it 'handles mixed positive and negative infinity' do
+      expect(described_class.equal?(Float::INFINITY, -Float::INFINITY)).to be false
+    end
+
+    it 'returns false for non-equal strings' do
+      expect(described_class.equal?('hello', 'world')).to be false
+    end
+
+    it 'handles empty arrays' do
+      expect(described_class.equal?([], [])).to be true
+    end
+
+    it 'handles empty hashes' do
+      expect(described_class.equal?({}, {})).to be true
+    end
+
+    it 'handles nested hashes' do
+      a = { point: { x: 1.0, y: 2.0 } }
+      b = { point: { x: 1.0 + 1e-10, y: 2.0 } }
+      expect(described_class.equal?(a, b)).to be true
+    end
+
+    it 'returns false for hashes with different number of keys' do
+      expect(described_class.equal?({ a: 1.0 }, { a: 1.0, b: 2.0 })).to be false
+    end
+
+    it 'handles large epsilon' do
+      expect(described_class.equal?(0.0, 100.0, epsilon: 200.0)).to be true
+    end
   end
 
   describe '.near?' do
     it 'behaves like equal?' do
       expect(described_class.near?(1.0, 1.0 + 1e-10)).to be true
+    end
+
+    it 'accepts custom epsilon' do
+      expect(described_class.near?(1.0, 1.5, epsilon: 1.0)).to be true
+    end
+
+    it 'returns false for distant values' do
+      expect(described_class.near?(1.0, 100.0)).to be false
     end
   end
 
@@ -70,6 +166,22 @@ RSpec.describe Philiprehberger::Approx do
 
     it 'includes values in error message' do
       expect { described_class.assert_near(1.0, 2.0) }.to raise_error(/expected 1.0 to be near 2.0/)
+    end
+
+    it 'includes epsilon in error message' do
+      expect { described_class.assert_near(1.0, 2.0, epsilon: 0.5) }.to raise_error(/epsilon: 0.5/)
+    end
+
+    it 'does not raise for near arrays' do
+      expect { described_class.assert_near([1.0, 2.0], [1.0, 2.0 + 1e-10]) }.not_to raise_error
+    end
+
+    it 'raises Error for distant arrays' do
+      expect { described_class.assert_near([1.0], [2.0]) }.to raise_error(described_class::Error)
+    end
+
+    it 'accepts custom epsilon' do
+      expect { described_class.assert_near(1.0, 1.5, epsilon: 1.0) }.not_to raise_error
     end
   end
 end
